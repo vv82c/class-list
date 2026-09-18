418b7a32a20ef363092171542ccb9fba6e6a8d15
LAINXIANG <vv82c@qq.com>
2026-09-18 14:18:55 +0800

    修复 PWA service worker 未注册

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
-import 'virtual:pwa-register';
+import { registerSW } from 'virtual:pwa-register';
 import App from './App';
 import './index.css';
+
+registerSW({ immediate: true });
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
